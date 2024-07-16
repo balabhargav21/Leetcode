ba031aa5c7f4dcf059c184e877b44637bbecdfdf
balabhargav21 <140328274+balabhargav21@users.noreply.github.com>
2024-07-16 22:56:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (10.28%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -1,8 +1,16 @@
 class Solution {
     public int largestAltitude(int[] gain) {
+        //initially the altitude is 0,
+        //here max represents the highest altitude 
+        //here alt represents the current altitude 
       int max =0;
       int alt =0;
      for(int i=0; i < gain.length; i++){
+        //current position is 0 and next point is -5 
+        //my cuurent alt is 0 + (-5);
+        //alt calculates the altitude and we need to return the highAltitude 
+        //we compare the alt and max
+        
         alt = gain[i] + alt;
         max = Math.max(alt,max);
          
