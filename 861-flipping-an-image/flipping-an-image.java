c9a65bb8753b51861f8b20041ac93d523af01855
balabhargav21 <140328274+balabhargav21@users.noreply.github.com>
2024-07-17 21:49:05 +0530

    Time: 1 ms (46.22%) | Memory: 44.7 MB (10.64%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -9,11 +9,10 @@ class Solution {
         }
         for(int i=0; i <n; i++){
             for(int j=0; j<n; j++){
-           result[i][j] = result[i][j] == 1 ? 0 : 1;
+             result[i][j] = result[i][j] == 1 ? 0 : 1;
             
             }
-
         }
-      return result;
+        return result;
     }
 }
