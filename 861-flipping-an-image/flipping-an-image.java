4ba6c42679b88dee9f5f9213171f447c3e19f3bc
balabhargav21 <140328274+balabhargav21@users.noreply.github.com>
2024-07-17 21:51:47 +0530

    Time: 1 ms (46.22%) | Memory: 43.9 MB (98.49%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.java
+++ 861-flipping-an-image/flipping-an-image.java
@@ -2,11 +2,14 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int n = image[0].length;
         int[][] result = new int[n][n];
+        //step 1 : we need reverse the each row horizontally in the given matrix
         for(int i=0 ; i < n; i++){
             for(int j=0; j<n; j++){
                 result[i][j] = image[i][n-j-1];
             }
         }
+        //after reversing each row we need to invert the result matrix
+        //it means replace 1 with 0 , 0 with 1
         for(int i=0; i <n; i++){
             for(int j=0; j<n; j++){
              result[i][j] = result[i][j] == 1 ? 0 : 1;
